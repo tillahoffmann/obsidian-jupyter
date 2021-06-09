5f413854e59a5620174cd0d36cc9f1a570198b18
Till Hoffmann <tillahoffmann@gmail.com>
2021-06-09 13:53:50 +0100

    Use persistent kernel for each document.

--- main.ts
+++ main.ts
@@ -7,7 +7,8 @@ import {
 	FileSystemAdapter,
 	MarkdownRenderer
 } from 'obsidian';
-import { exec } from 'child_process';
+import { spawn, ChildProcess } from 'child_process';
+import { v4 as uuid } from 'uuid';
 
 interface JupyterPluginSettings {
 	pythonInterpreter: string;
@@ -17,12 +18,69 @@ const DEFAULT_SETTINGS: JupyterPluginSettings = {
 	pythonInterpreter: 'python'
 }
 
+
+class JupyterClient {
+	process: ChildProcess;
+	promises: Map<string, any>;
+	stdinParts: string[];
+
+	processStdIn(data: any) {
+		this.stdinParts.push(data.toString());
+		if (this.stdinParts.last().endsWith('\n')) {
+			let response = JSON.parse(this.stdinParts.join(''));
+			this.stdinParts = [];
+			let promise = this.promises.get(response.id);
+			if (promise === undefined) {
+				console.error(`received response for unrecognised promise: ${response.id}`);
+				return;
+			}
+			promise(response.body);
+		}
+	}
+
+	processStdErr(data: any) {
+		console.log(data.toString());
+	}
+
+	constructor (cmd: string, args?: string[], options?: any) {
+		this.process = spawn(cmd, args, options);
+		this.process.stdout.on('data', this.processStdIn.bind(this));
+		this.process.stderr.on('data', this.processStdErr.bind(this));
+		this.process.on('error', console.log);
+		this.promises = new Map();
+		this.stdinParts = [];
+	}
+
+	async request(body: any): Promise<any> {
+		// Generate a random identifier.
+		var id = uuid();
+		// Send the request (\n terminated to make sure it gets picked up by the python process).
+		var data = JSON.stringify({id: id, body: body});
+		this.process.stdin.write(data + '\n');
+		// Create a resolvable promise and store it against the id.
+		var resolve;
+		var reject;
+		var promise = new Promise((resolve_, reject_) => {
+			resolve = resolve_;
+			reject = reject_;
+		})
+		this.promises.set(id, resolve);
+		return promise;
+	}
+
+	stop() {
+		this.process.stdin.end();
+	}
+}
+
 export default class JupyterPlugin extends Plugin {
 	settings: JupyterPluginSettings;
+	clients: Map<string, JupyterClient>;
 
 	async postprocessor(src: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) {
 		// Render the code using the default renderer for python.
-		await MarkdownRenderer.renderMarkdown('```python\n' + src + '```', el, '', this.app.workspace.activeLeaf.view);
+		await MarkdownRenderer.renderMarkdown('```python\n' + src + '```', el, '',
+											  this.app.workspace.activeLeaf.view);
 
 		// Needed for positioning of the button and hiding Jupyter prompts.
 		el.classList.add('obsidian-jupyter');
@@ -34,11 +92,10 @@ export default class JupyterPlugin extends Plugin {
 		button.className = 'obsidian-jupyter-run-button';
 		button.addEventListener('click', () => {
 			button.innerText = 'Running...';
-			let options = {
-				cwd: (this.app.vault.adapter as FileSystemAdapter).getBasePath(),
-			};
-			let cmd = `${this.settings.pythonInterpreter} ${this.app.vault.configDir}/plugins/obsidian-jupyter/convert.py`;
-			let child = exec(cmd, options, (error, stdout, stderr) => {
+			this.getJupyterClient(ctx.docId).request({
+				command: 'execute',
+				source: src,
+			}).then(response => {
 				// Find the div to paste the output into or create it if necessary.
 				var output = el.querySelector('div.obsidian-jupyter-output');
 				if (output == null) {
@@ -46,13 +103,24 @@ export default class JupyterPlugin extends Plugin {
 					output.classList.add('obsidian-jupyter-output');
 				}
 				// Paste the output and reset the button.
-				output.innerHTML = stdout;
+				output.innerHTML = response;
 				button.innerText = 'Run';
 			});
-			// Send the code to execute to the conversion script.
-			child.stdin.write(src);
-			child.stdin.end();
 		});
+	}
+
+	getJupyterClient(docId: string): JupyterClient {
+		let client = this.clients.get(docId);
+		if (client === undefined) {
+			let options = {
+				cwd: (this.app.vault.adapter as FileSystemAdapter).getBasePath(),
+			};
+			let path = `${this.app.vault.configDir}/plugins/obsidian-jupyter/obsidian-jupyter.py`;
+			client = new JupyterClient(this.settings.pythonInterpreter, [path, docId], options);
+			this.clients.set(docId, client);
+			console.log(`created new client for doc ${docId}`);
+		}
+		return client;
 	}
 
 	async onload() {
@@ -62,10 +130,15 @@ export default class JupyterPlugin extends Plugin {
 
 		this.addSettingTab(new JupyterSettingTab(this.app, this));
 		this.registerMarkdownCodeBlockProcessor('jupyter', this.postprocessor.bind(this));
+		this.clients = new Map();
 	}
 
 	onunload() {
 		console.log('unloading jupyter plugin');
+		this.clients.forEach((client, docId) => {
+			console.log(`stopping client for doc ${docId}...`);
+			client.stop();
+		});
 	}
 
 	async loadSettings() {
---
+++ obsidian-jupyter.py
@@ -0,0 +1,65 @@
+import argparse
+import sys
+from jupyter_client import KernelManager
+import nbformat
+from nbconvert import HTMLExporter
+from nbclient import NotebookClient
+from nbclient.exceptions import CellExecutionError
+import json
+import logging
+
+
+# Parse input arguments.
+parser = argparse.ArgumentParser()
+parser.add_argument('document_id')
+args = parser.parse_args()
+
+# Set up a logger that writes to stderr.
+logging.basicConfig(level='INFO')
+logger = logging.getLogger('obsidian-jupyter')
+logger.info('started server for document %s', args.document_id)
+
+# Create a notebook and kernel.
+cell = nbformat.v4.new_code_cell()
+nb = nbformat.v4.new_notebook(cells=[cell])
+km = KernelManager()
+client = NotebookClient(nb, km)
+
+# Use line buffering.
+sys.stdout.reconfigure(line_buffering=True)
+
+try:
+    # Respond to each request.
+    for request in sys.stdin:
+        # Load the request and generate a response with matching id.
+        logger.info('received request: %s', request)
+        request = json.loads(request)
+        request_body = request['body']
+        response = {
+            'id': request['id'],
+        }
+        # Execute a cell.
+        if request_body['command'] == 'execute':
+            cell['source'] = request_body['source']
+            try:
+                nb = client.execute(nb)
+            except CellExecutionError as ex:
+                logger.info('cell failed to execute: %s', ex)
+            html_exporter = HTMLExporter(template_name='basic')
+            (response_body, resources) = html_exporter.from_notebook_node(nb)
+        elif request_body['command'] == 'restart_kernel':
+            km.restart_kernel()
+            response_body = ''
+
+        # Pass the response back.
+        response['body'] = response_body
+        response = json.dumps(response)
+        sys.stdout.write(response + '\n')
+        sys.stdout.flush()
+        logger.info('sent response: %s', response)
+finally:
+    # Clean up the kernel.
+    if km.is_alive:
+        km.shutdown_kernel()
+
+logger.info('exiting...')
